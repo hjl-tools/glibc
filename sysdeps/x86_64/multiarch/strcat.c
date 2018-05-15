269c07c45414c92656d6af6fbcf84378d5f8b54c
H.J. Lu <hjl.tools@gmail.com>
2018-05-15 13:53:44 -0700

    libcpu-rt-c/x86-64: Add strcat
    	* sysdeps/x86_64/Makefile (cpu-rt-c-routines): Add strcat.
    	* sysdeps/x86_64/multiarch/Makefile
    	(libcpu-rt-c-sysdep_routines): Add strcat-sse2,
    	strcat-sse2-unaligned and strcat-ssse3.
    	* sysdeps/x86_64/strcat.S: Support libcpu-rt-c.
    	* sysdeps/x86_64/multiarch/strcat-sse2-unaligned.S: Likewise.
    	* sysdeps/x86_64/multiarch/strcat-sse2.S: Likewise.
    	* sysdeps/x86_64/multiarch/strcat-ssse3.S: Likewise.
    	* sysdeps/x86_64/multiarch/strcat.c: Likewise.

--- sysdeps/x86_64/multiarch/strcat.c
+++ sysdeps/x86_64/multiarch/strcat.c
@@ -18,7 +18,7 @@
    <http://www.gnu.org/licenses/>.  */
 
 /* Define multiple versions only for the definition in libc.  */
-#if IS_IN (libc)
+#if IS_IN (libc) || IS_IN (libcpu_rt_c)
 # define strcat __redirect_strcat
 # include <string.h>
 # undef strcat
@@ -28,7 +28,7 @@
 
 libc_ifunc_redirected (__redirect_strcat, strcat, IFUNC_SELECTOR ());
 
-# ifdef SHARED
+# if defined SHARED && !IS_IN (libcpu_rt_c)
 __hidden_ver1 (strcat, __GI_strcat, __redirect_strcat)
   __attribute__ ((visibility ("hidden")));
 # endif
