433fe51697f4d2c467f7b23312c3da4bef497a04
H.J. Lu <hjl.tools@gmail.com>
2018-05-15 05:04:08 -0700

    libcpu-rt-c/x86-64: Add strchr
    	* sysdeps/x86_64/Makefile (cpu-rt-c-routines): Add strchr.
    	* sysdeps/x86_64/multiarch/Makefile
    	(libcpu-rt-c-sysdep_routines): Add strchr-sse2-no-bsf,
    	strchr-sse2 and strchr-avx2.
    	* sysdeps/x86_64/multiarch/strchr-avx2.S: Support libcpu-rt-c.
    	* sysdeps/x86_64/multiarch/strchr-sse2-no-bsf.S: Likewise.
    	* sysdeps/x86_64/multiarch/strchr-sse2.S: Likewise.
    	* sysdeps/x86_64/multiarch/strchr.c: Likewise.

--- sysdeps/x86_64/multiarch/strchr.c
+++ sysdeps/x86_64/multiarch/strchr.c
@@ -18,7 +18,7 @@
    <http://www.gnu.org/licenses/>.  */
 
 /* Define multiple versions only for the definition in libc.  */
-#if IS_IN (libc)
+#if IS_IN (libc) || IS_IN (libcpu_rt_c)
 # define strchr __redirect_strchr
 # include <string.h>
 # undef strchr
@@ -48,7 +48,7 @@ IFUNC_SELECTOR (void)
 
 libc_ifunc_redirected (__redirect_strchr, strchr, IFUNC_SELECTOR ());
 weak_alias (strchr, index)
-# ifdef SHARED
+# if defined SHARED && !IS_IN (libcpu_rt_c)
 __hidden_ver1 (strchr, __GI_strchr, __redirect_strchr)
   __attribute__((visibility ("hidden")));
 # endif
