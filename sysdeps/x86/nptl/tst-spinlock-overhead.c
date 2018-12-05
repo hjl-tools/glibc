0785367b1aa85daefe4071e108809454d38dc267
H.J. Lu <hjl.tools@gmail.com>
2018-12-05 13:00:04 -0800

    Add tst-spinlock-overhead.c and tst-numa-spinlock-overhead.c
    1. tst-spinlock-overhead
    
    On CFL:
    
    Number of threads:    2, Total time      111692114, Overhead: 1.35
    Number of threads:    4, Total time      437096222, Overhead: 2.64
    Number of threads:    8, Total time     1566989478, Overhead: 4.73
    Number of threads:   12, Total time     3735663036, Overhead: 7.51
    
    On BDX:
    
    Number of threads:    2, Total time      161928910, Overhead: 2.68
    Number of threads:    4, Total time      632319301, Overhead: 5.22
    Number of threads:    8, Total time     2607285614, Overhead: 10.77
    Number of threads:   16, Total time    12960115272, Overhead: 26.77
    Number of threads:   32, Total time    69437649115, Overhead: 71.70
    Number of threads:   48, Total time   131934124605, Overhead: 90.82
    Number of threads:   56, Total time   171965209489, Overhead: 101.47
    
    on SKX:
    
    Number of threads:    2, Total time      210957738, Overhead: 2.83
    Number of threads:    4, Total time      700807292, Overhead: 4.71
    Number of threads:    8, Total time     2730139430, Overhead: 9.17
    Number of threads:   16, Total time    12286303676, Overhead: 20.63
    Number of threads:   32, Total time    71034964166, Overhead: 59.63
    Number of threads:   64, Total time   374564752366, Overhead: 157.23
    Number of threads:   80, Total time   564485071330, Overhead: 189.56
    Number of threads:   96, Total time   830032415132, Overhead: 232.27
    Number of threads:  112, Total time  1118021304438, Overhead: 268.17
    
    2. tst-numa-spinlock-overhead
    
    On CFL:
    
    Number of threads:    2, Total time      143721672, Overhead: 1.52
    Number of threads:    4, Total time      404509056, Overhead: 2.14
    Number of threads:    8, Total time      947638656, Overhead: 2.51
    Number of threads:   12, Total time     1594749872, Overhead: 2.81
    
    On BDX:
    
    Number of threads:    2, Total time      168167714, Overhead: 2.42
    Number of threads:    4, Total time      482525975, Overhead: 3.47
    Number of threads:    8, Total time     1397432259, Overhead: 5.03
    Number of threads:   16, Total time     4078305832, Overhead: 7.34
    Number of threads:   32, Total time    12059269166, Overhead: 10.85
    Number of threads:   48, Total time    23553679167, Overhead: 14.13
    Number of threads:   56, Total time    29682472170, Overhead: 15.26
    
    On SKX:
    
    Number of threads:    2, Total time      207803788, Overhead: 2.13
    Number of threads:    4, Total time      687834998, Overhead: 3.53
    Number of threads:    8, Total time     1643241490, Overhead: 4.22
    Number of threads:   16, Total time     4999757806, Overhead: 6.42
    Number of threads:   32, Total time    15625584082, Overhead: 10.03
    Number of threads:   64, Total time    48207310808, Overhead: 15.47
    Number of threads:   80, Total time    73146275124, Overhead: 18.78
    Number of threads:   96, Total time   101618181262, Overhead: 21.74
    Number of threads:  112, Total time   138927813380, Overhead: 25.47

---
+++ sysdeps/x86/nptl/tst-spinlock-overhead.c
@@ -0,0 +1,287 @@
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE
+#endif
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sched.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <sys/param.h>
+#include <errno.h>
+
+#define memory_barrier() __asm ("" ::: "memory")
+#define pause() __asm  ("rep ; nop" ::: "memory")
+
+#define CACHELINE_SIZE	64
+#define CACHE_ALIGNED	__attribute__((aligned(CACHELINE_SIZE)))
+
+#define constant_time 5
+unsigned long g_val CACHE_ALIGNED;
+unsigned long g_val2 CACHE_ALIGNED;
+unsigned long g_val3 CACHE_ALIGNED;
+unsigned long cmplock CACHE_ALIGNED;
+struct count
+{
+  unsigned long long total;
+  unsigned long long spinlock;
+  unsigned long long wall;
+} __attribute__((aligned(128)));
+
+struct count *gcount;
+
+/* The time consumed by one update is about 200 TSCs.  */
+static int delay_time_unlocked = 400;
+
+struct
+{
+  pthread_spinlock_t testlock;
+  char pad[64];
+} test CACHE_ALIGNED;
+
+#define lock() pthread_spin_lock(&test.testlock)
+#define unlock() pthread_spin_unlock(&test.testlock)
+
+static void
+__attribute__((constructor))
+init_spin (void)
+{
+  pthread_spin_init (&test.testlock, 0);
+}
+
+struct ops
+{
+  void *(*test) (void *arg);
+  void (*print_thread) (void *res, int);
+} *ops;
+
+struct stats_result
+{
+  unsigned long num;
+};
+
+void *work_thread (void *arg);
+
+void test_threads (int numthreads, unsigned long time);
+
+#define iterations (10000 * 5)
+
+static volatile int start_thread;
+static volatile int stop_flag;
+
+
+/* Delay some fixed time */
+static void
+delay_tsc (unsigned n)
+{
+  unsigned long long start, current, diff;
+  unsigned int aux;
+  start = __builtin_ia32_rdtscp (&aux);
+  while (1)
+    {
+      current = __builtin_ia32_rdtscp (&aux);
+      diff = current - start;
+      if (diff < n)
+	pause ();
+      else
+	break;
+    }
+}
+
+static void
+wait_a_bit (int delay_time)
+{
+  if (delay_time > 0)
+    delay_tsc (delay_time);
+}
+
+static void
+work_todo (void)
+{
+  unsigned long ret;
+  g_val = g_val + 1;
+  g_val2 = g_val2 + 1;
+  ret = __sync_val_compare_and_swap (&cmplock, 0, 1);
+  g_val3 = g_val3 + 1 + ret;
+}
+
+void *
+work_thread (void *arg)
+{
+  long i;
+  unsigned long pid = (unsigned long) arg;
+  struct stats_result *res;
+  unsigned long long start, end;
+  int err_ret = posix_memalign ((void **)&res, CACHELINE_SIZE,
+				roundup (sizeof (*res), CACHELINE_SIZE));
+  if (err_ret)
+    {
+      printf ("posix_memalign failure: %s\n", strerror (err_ret));
+      exit (err_ret);
+    }
+  long num = 0;
+
+  while (!start_thread)
+    pause ();
+
+  unsigned int aux;
+  start = __builtin_ia32_rdtscp (&aux);
+  for (i = 0; i < iterations; i++)
+    {
+      lock();
+      work_todo ();
+      unlock ();
+      wait_a_bit (delay_time_unlocked);
+      num++;
+    }
+  end = __builtin_ia32_rdtscp (&aux);
+  gcount[pid].total = end - start;
+  res->num = num;
+
+  return res;
+}
+
+
+void
+init_global_data(void)
+{
+  g_val = 0;
+  g_val2 = 0;
+  g_val3 = 0;
+  cmplock = 0;
+}
+
+void
+test_threads (int numthreads, unsigned long time)
+{
+  start_thread = 0;
+  stop_flag = 0;
+
+  memory_barrier ();
+
+  pthread_t thr[numthreads];
+  void *res[numthreads];
+  int i;
+
+  init_global_data ();
+  for (i = 0; i < numthreads; i++)
+    {
+      pthread_attr_t attr;
+      pthread_attr_init (&attr);
+      cpu_set_t set;
+      CPU_ZERO (&set);
+      (void)CPU_SET (i, &set);
+      pthread_attr_setaffinity_np (&attr, sizeof(cpu_set_t), &set);
+      pthread_create (&thr[i], &attr, ops->test, (void *)(long)i);
+    }
+
+  memory_barrier ();
+  start_thread = 1;
+  memory_barrier ();
+  sched_yield ();
+
+  if (time)
+    {
+      struct timespec ts =
+	{
+	  ts.tv_sec = time,
+	  ts.tv_nsec = 0
+	};
+      clock_nanosleep (CLOCK_MONOTONIC, 0, &ts, NULL);
+      memory_barrier ();
+      stop_flag = 1;
+    }
+
+  for (i = 0; i < numthreads; i++) {
+    pthread_join (thr[i], (void *)&res[i]);
+    free (res[i]);
+  }
+
+}
+
+struct ops hashwork_ops =
+{
+  .test = work_thread,
+};
+
+struct ops *ops;
+
+static struct count
+total_cost (int numthreads)
+{
+  int i;
+  unsigned long long total = 0;
+  unsigned long long spinlock = 0;
+
+  memset (gcount, 0, sizeof(gcount[0]) * numthreads);
+
+  unsigned long long start, end, diff;
+  unsigned int aux;
+
+  start = __builtin_ia32_rdtscp (&aux);
+  test_threads (numthreads, constant_time);
+  end = __builtin_ia32_rdtscp (&aux);
+  diff = end - start;
+
+  for (i = 0; i < numthreads; i++)
+    {
+      total += gcount[i].total;
+      spinlock += gcount[i].spinlock;
+    }
+
+  struct count cost = { total, spinlock, diff };
+  return cost;
+}
+
+int
+main (void)
+{
+  int numthreads = sysconf (_SC_NPROCESSORS_ONLN);
+  if (numthreads < 8)
+    return 1;
+
+  ops = &hashwork_ops;
+
+  int err_ret = posix_memalign ((void **)&gcount, 4096,
+				sizeof(gcount[0]) * numthreads);
+  if (err_ret)
+    {
+      printf ("posix_memalign failure: %s\n", strerror (err_ret));
+      exit (err_ret);
+    }
+
+  struct count cost, cost1;
+  double overhead;
+  int i, last;
+  cost1 = total_cost (1);
+  for (last = i = 2; i <= numthreads;)
+    {
+      last = i;
+      cost = total_cost (i);
+      overhead = cost.total;
+      overhead /= i;
+      overhead /= cost1.total;
+      printf ("Number of threads: %4d, Total time %14lld, Overhead: %.2f\n",
+	      i, cost.total, overhead);
+      if ((i * 2) < numthreads)
+	i = i * 2;
+      else
+	i = i + 16;
+    }
+
+  if (last != numthreads)
+    {
+      i = numthreads;
+      cost = total_cost (i);
+      overhead = cost.total;
+      overhead /= i;
+      overhead /= cost1.total;
+      printf ("Number of threads: %4d, Total time %14lld, Overhead: %.2f\n",
+	      i, cost.total, overhead);
+    }
+
+  free (gcount);
+  return 0;
+}
